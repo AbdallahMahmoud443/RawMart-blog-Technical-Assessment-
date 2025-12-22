8e0027b2bf4fc8386dbc79c70b62d21f4767ebce
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-12-22 04:50:09 +0200

    commit 3:refactor(auth):add image column to schema of user model with some modification

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique()->index();
             $table->string('password');
+            $table->string('image');
+
             $table->rememberToken();
+
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
 
